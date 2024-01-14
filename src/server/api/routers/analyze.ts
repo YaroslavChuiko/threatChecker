f3e4b808b1a378658f7a3ad28b8a1da732d5ed44
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2024-01-14 17:47:21 +0200

    add relative weights

--- src/server/api/routers/analyze.ts
+++ src/server/api/routers/analyze.ts
@@ -8,72 +8,89 @@ type DangerousFunction = {
   name: string;
   regExp: RegExp;
   description?: string;
+  weight: 1 | 2 | 3 | 4 | 5;
 };
 
 const dangerousFunctions: DangerousFunction[] = [
-  // {
-  //   name: "indexOf", // String.prototype.indexOf()
-  //   regExp: /\.indexOf\(.*\)/g,
-  // },
-  // {
-  //   name: "charAt", // String.prototype.charAt()
-  //   regExp: /\.charAt\(.*\)/g,
-  // },
-  // {
-  //   name: "split", // String.prototype.split()
-  //   regExp: /\.split\(.*\)/g,
-  // },
-  // {
-  //   name: "String.fromCharCode", // String.fromCharCode()
-  //   regExp: /String\.fromCharCode\(.*\)/g,
-  // },
-  // {
-  //   name: "String.charCodeAt", // String.charCodeAt()
-  //   regExp: /String\.charCodeAt\(.*\)/g,
-  // },
+  {
+    name: "indexOf", // String.prototype.indexOf()
+    regExp: /\.indexOf\(.*\)/g,
+    weight: 1,
+  },
+  {
+    name: "charAt", // String.prototype.charAt()
+    regExp: /\.charAt\(.*\)/g,
+    weight: 1,
+  },
+  {
+    name: "split", // String.prototype.split()
+    regExp: /\.split\(.*\)/g,
+    weight: 1,
+  },
+  {
+    name: "String.fromCharCode", // String.fromCharCode()
+    regExp: /String\.fromCharCode\(.*\)/g,
+    weight: 1,
+  },
+  {
+    name: "String.charCodeAt", // String.charCodeAt()
+    regExp: /String\.charCodeAt\(.*\)/g,
+    weight: 1,
+  },
   {
     name: "eval", // eval()
     regExp: /eval\(.*\)/g,
+    weight: 5,
   },
   {
     name: "setTimeout", // setTimeout()
     regExp: /setTimeout\(.*\)/g,
+    weight: 2,
   },
   {
     name: "setInterval", // setInterval()
     regExp: /setInterval\(.*\)/g,
+    weight: 2,
   },
   {
     name: "document.write", // document.write()
     regExp: /document\.write\(.*\)/g,
+    weight: 4,
   },
   {
     name: "document.writeln", // document.writeln()
     regExp: /document\.writeln\(.*\)/g,
+    weight: 4,
   },
-  // {
-  //   name: "appendChild", // element.appendChild()
-  //   regExp: /\.appendChild\(.*\)/g,
-  // },
+  {
+    name: "appendChild", // element.appendChild()
+    regExp: /\.appendChild\(.*\)/g,
+    weight: 2,
+  },
   {
     name: "innerHTML", // element.innerHTML
     regExp: /\.innerHTML\s*=/g,
+    weight: 4,
   },
   {
     name: "location.assign", // location.assign()
     regExp: /location\.assign\(.*\)/g,
+    weight: 4,
   },
   {
     name: "location.replace", // location.replace()
     regExp: /location\.replace\(.*\)/g,
+    weight: 4,
   },
   {
     name: "unescape", // unescape()
     regExp: /unescape\(.*\)/g,
+    weight: 5,
   },
   {
     name: "XMLHttpRequest", // XMLHttpRequest
     regExp: /XMLHttpRequest\(/g,
+    weight: 3,
   },
 ];
 
@@ -81,62 +98,31 @@ export const analyzeRouter = createTRPCRouter({
   scan: publicProcedure
     .input(z.object({ url: z.string().url({ message: "Invalid url" }) }))
     .mutation(async ({ ctx, input }) => {
-      const origin = new URL(input.url).origin;
-      const data = await fetch(input.url);
-      const text = await data.text();
-      const $ = cheerio.load(text);
-
-      const scriptTagCodeArray: string[] = $("script:not([src])")
-        .map((i, el) => {
-          // console.log("script", $(el).toString());
-          // // console.log(i, $(el).contents().toString());
-          // console.log(i, $(el).text());
-          // console.log("\n");
-
-          return $(el).contents().toString();
-        })
-        .toArray();
-
-      const fileDownloadUrls: string[] = $("script[src]")
-        .map((i, el) => {
-          const url = $(el).attr("src");
-          return url?.startsWith("http") ? url : `${origin}${url}`;
-        })
-        .toArray()
-        .filter((url) => z.string().url().safeParse(url).success);
-
-      console.log("scriptSources", fileDownloadUrls);
-
-      const downloadedScripts = await Promise.allSettled(
-        fileDownloadUrls.map(async (url) => {
-          const data = await fetch(url);
-          const text = await data.text();
-          return text;
-        }),
-      );
-
-      console.log(
-        "downloadedScripts",
-        downloadedScripts.map((r) => r.status),
-      );
+      const { scriptURLList, scriptsToScan } = await extractScripts(input.url);
+      const downloadedScripts = await downloadScripts(scriptURLList);
 
       downloadedScripts.forEach((result) => {
         if (result.status === "fulfilled") {
-          scriptTagCodeArray.push(result.value);
+          scriptsToScan.push(result.value);
         }
       });
 
-      console.log("scriptTagCodeArray.length", scriptTagCodeArray.length);
+      //* start refactoring here after this db update
+      const dangerousFunctionsWithRelativeWeight =
+        calculateRelativeWeights(dangerousFunctions);
 
-      const result = new Map<string, DangerousFunction>();
+      const result = new Map<
+        string,
+        (typeof dangerousFunctionsWithRelativeWeight)[number]
+      >();
 
-      for (const item of dangerousFunctions) {
+      for (const item of dangerousFunctionsWithRelativeWeight) {
         const key = JSON.stringify(item); //* mb use id from db
         if (result.has(key)) {
           continue;
         }
 
-        for (const script of scriptTagCodeArray) {
+        for (const script of scriptsToScan) {
           if (script.search(item.regExp) != -1) {
             result.set(key, item);
           }
@@ -144,10 +130,71 @@ export const analyzeRouter = createTRPCRouter({
       }
 
       console.log(`${result.size} of ${dangerousFunctions.length}`);
+      console.log(
+        "relativeWeight",
+        Array.from(result.values()).reduce(
+          (acc, item) => acc + item.relativeWeight,
+          0,
+        ),
+      );
       console.log(result);
 
       return {
         greeting: `Hello ${input.url}`,
+        vulnerabilityCoef: Array.from(result.values()).reduce(
+          (acc, item) => acc + item.relativeWeight,
+          0,
+        ),
       };
     }),
 });
+
+const extractScripts = async (url: string) => {
+  const origin = new URL(url).origin;
+  const siteToScan = await fetch(url);
+  const HTMLToScan = await siteToScan.text();
+  const $ = cheerio.load(HTMLToScan);
+
+  const scriptsToScan: string[] = $("script:not([src])")
+    .map((i, el) => $(el).contents().toString())
+    .toArray();
+
+  const scriptURLList: string[] = $("script[src]")
+    .map((i, el) => {
+      const url = $(el).attr("src");
+      return url?.startsWith("http") ? url : `${origin}${url}`;
+    })
+    .toArray()
+    .filter((url) => z.string().url().safeParse(url).success);
+
+  console.log("scriptURLList", scriptURLList);
+  console.log("scriptsToScan.length", scriptsToScan.length);
+
+  return { scriptsToScan, scriptURLList };
+};
+
+const downloadScripts = async (scriptURLList: string[]) => {
+  const downloadedScripts = await Promise.allSettled(
+    scriptURLList.map(async (url) => {
+      const data = await fetch(url);
+      const text = await data.text();
+      return text;
+    }),
+  );
+
+  console.log(
+    "downloadedScripts",
+    downloadedScripts.map((r) => r.status),
+  );
+
+  return downloadedScripts;
+};
+
+const calculateRelativeWeights = (functionsArr: DangerousFunction[]) => {
+  const weightSum = functionsArr.reduce((acc, item) => acc + item.weight, 0);
+
+  return functionsArr.map((item) => ({
+    ...item,
+    relativeWeight: item.weight / weightSum,
+  }));
+};
