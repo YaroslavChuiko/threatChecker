1f8ea25641957a96636ed5b2373a0aebe07f88c9
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2024-01-25 20:21:13 +0200

    [frontend] style: update logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
-import Logo from "~/assets/MJ_LogoType.png";
 import { ROUTES } from "~/routes";
+import EyeIcon from "../icons/EyeIcon/Eye";
 
 const links = [
   { href: ROUTES.PUBLIC.HOME, label: "Home" },
@@ -10,21 +9,22 @@ const links = [
 
 const Header = () => {
   return (
-    <header className="absolute inset-x-0 top-0 z-40 flex items-center justify-between px-14 pb-8 pt-6 text-white">
+    <header className="absolute inset-x-0 top-0 z-40 flex items-center justify-between px-14 pb-4 pt-4 text-white">
       <Link
-        className="absolute right-1/2 translate-x-1/2"
+        className="flex items-center"
         href={ROUTES.PUBLIC.HOME}
       >
-        <Image width={230} height={30} src={Logo.src} alt="" />
+        <EyeIcon className="w-8 h-8" />
+        <span className="pl-2 font-mono leading-none text-lg">ThreatMinder</span>
       </Link>
-      <nav className="">
-        <ul className="flex items-center gap-4 font-sans font-bold">
+      <nav className="absolute right-1/2 translate-x-1/2">
+        <ul className="flex items-center gap-6 font-sans font-bold">
           {links.map(({ href, label }) => (
             <li
               key={href}
               // className="rounded-full bg-white/10 px-5 py-2 font-sans font-bold transition hover:bg-white/20"
             >
-              <Link href={href}>{label}</Link>
+              <Link href={href} className="p-2">{label}</Link>
             </li>
           ))}
         </ul>
@@ -35,5 +35,4 @@ const Header = () => {
     </header>
   );
 };
-
 export default Header;
