c6acdaa8edc3a499c4787279107256d62d2bd289
YaroslavChuiko <jarik.tchuicko@gmail.com>
2024-02-16 22:32:41 +0200

    [frontend] chore: create Footer component

--- src/app/(routes)/(rest)/layout.tsx
+++ src/app/(routes)/(rest)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "~/components/Footer";
 import Header from "~/components/Header";
 import { getServerAuthSession } from "~/server/auth";
 
@@ -9,13 +10,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
     <>
       <Header session={session}/>
       <main>{children}</main>
-      <footer className="absolute bottom-4 inset-x-4 h-16 px-6 py-2 flex items-end bg-[#ff3845] select-none">
-        <div className="flex w-full items-center font-medium justify-between text-[#0E0E17]">
-          <div>INTERFACE LOADED</div>
-          <div className="pr-60">PROVIDED BY NEXUS NETWORK V10.8</div>
-          <div>BUILD 6.47.48441.R15</div>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="absolute inset-x-4 bottom-4 flex h-16 select-none items-end bg-mainColor px-6 py-2">
+      <div className="flex w-full items-center justify-between font-medium text-secondaryColor">
+        <div>INTERFACE LOADED</div>
+        <div className="pr-60">PROVIDED BY NEXUS NETWORK V10.8</div>
+        <div>BUILD 6.47.48441.R15</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
